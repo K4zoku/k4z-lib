2f3b120c15351c48138c36154e926ecb21784210
K4zoku <kazoku0356@gmail.com>
2022-01-26 22:26:24 +0700

    optimized imports

--- k4z-lib-event/src/test/java/SimpleEventTest.java
+++ k4z-lib-event/src/test/java/SimpleEventTest.java
@@ -2,8 +2,6 @@ import com.github.k4zoku.lib.event.EventHandler;
 import com.github.k4zoku.lib.event.EventManager;
 import com.github.k4zoku.lib.event.Listener;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.concurrent.atomic.AtomicReference;
 
