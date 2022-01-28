19a0aa815aaccd103484c8351f2baed9aebdedc5
K4zoku <kazoku0356@gmail.com>
2022-01-28 17:03:27 +0700

    added javadocs for Configuration

--- k4z-lib-configuration/k4z-lib-configuration-base/src/main/java/com/github/k4zoku/configuration/base/Configuration.java
+++ k4z-lib-configuration/k4z-lib-configuration-base/src/main/java/com/github/k4zoku/configuration/base/Configuration.java
@@ -1,9 +1,45 @@
 package com.github.k4zoku.configuration.base;
 
 public interface Configuration {
+
+    /**
+     * Get options for the configuration.
+     *
+     * @return the options
+     */
     ConfigurationOptions options();
+
+    /**
+     * Get the configuration node at the given path.
+     *
+     * @param path the path
+     * @return the configuration node
+     */
     ConfigurationNode getNode(String path);
+
+    /**
+     * Get value of the configuration node at the given path.
+     *
+     * @param path the path
+     * @return the value
+     */
     Object get(String path);
+
+    /**
+     * Get value of the configuration node at the given path.
+     *
+     * @param path the path
+     * @param def the default value
+     * @return the value, or the default value if the node does not exist
+     */
     Object get(String path, Object def);
+
+    /**
+     * Set value of the configuration node at the given path.
+     *
+     * @param path the path
+     * @param value the value
+     */
     void set(String path, Object value);
+
 }
